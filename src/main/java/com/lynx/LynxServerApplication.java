4862f710045d99f0f0a8ee52b776bb3508861d16
emrekgn <emrekaganakkaya@gmail.com>
2017-07-10 15:08:02 +0300

    Initial commit

---
+++ src/main/java/com/lynx/LynxServerApplication.java
@@ -0,0 +1,12 @@
+package com.lynx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LynxServerApplication.class, args);
+	}
+}
